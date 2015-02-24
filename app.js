fd7897bb705b9885f01c0565038073826ad5357a
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-02-24 17:54:14 -0600

    Add support to azk

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var sendgrid_username   = process.env.SENDGRID_USERNAME;
 var sendgrid_password   = process.env.SENDGRID_PASSWORD;
 sendgrid                = require('sendgrid')(sendgrid_username, sendgrid_password);
 
-var port        = parseInt(process.env.PORT) || 3000;
+var port        = parseInt(process.env.HTTP_PORT) || 3000;
 var Hapi        = require('hapi');
 server          = new Hapi.Server(+port, '0.0.0.0', { cors: true });
 
